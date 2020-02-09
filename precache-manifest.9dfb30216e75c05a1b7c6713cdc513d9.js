b9bb02b559e82b39659ca83d09b08f3e9d7f1bd5
mvtram <mvenkat0897@gmail.com>
2020-02-10 02:23:59 +0530

    Updates

---
+++ precache-manifest.9dfb30216e75c05a1b7c6713cdc513d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57575717cd61925e5ec208f21edfacc7",
+    "url": "/new-game/index.html"
+  },
+  {
+    "revision": "584f1c096b1c5a7b03f5",
+    "url": "/new-game/static/css/main.e216b23f.chunk.css"
+  },
+  {
+    "revision": "14b37198b3654b550f6a",
+    "url": "/new-game/static/js/2.635a84f9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/new-game/static/js/2.635a84f9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "584f1c096b1c5a7b03f5",
+    "url": "/new-game/static/js/main.33de2aaf.chunk.js"
+  },
+  {
+    "revision": "1190caf663cc9de6fac1",
+    "url": "/new-game/static/js/runtime-main.823cfab3.js"
+  }
+]);
